bc95075debdb0efd29c5d2b0809ecf3d159f56c7
F.J. de Torres <fj.detorres@gmail.com>
2025-03-05 17:05:11 +0100

    v.claude.ai

--- queue.py
+++ queue.py
@@ -1,10 +1,39 @@
+from os import system
+
+class QueueError(Exception):
+    def __init__(self, message="Empty list"):
+        self.message = message
+        super().__init__(self.message)
+
+    def __str__(self):
+        return self.message
+
 class Queue():
     def __init__(self):
-        queue_list = list()
+        self.queue_list = list()
     
-    def put(self,element):
-        self.queue_list.append(element)
+    def put(self, element):
+        self.queue_list.insert(0, element)
     
     def get(self):
-        if len(self.queue_list)>0:
-            val = self.queue_list.pop(0) # type: ignore
+        if len(self.queue_list) > 0:
+            val = self.queue_list.pop(0)
+            return val
+        else:
+            raise QueueError("Empty list error!")
+
+if __name__ == "__main__":
+    system("cls || clear")
+    
+    que = Queue()
+    que.put(1)
+    que.put("Perro")
+    que.put(False)
+    
+    try:
+        for i in range(4):
+            print(que.get())
+    except QueueError as e:
+        print("Queue error!", str(e))
+    except Exception:
+        print("Warning: Non tracked error!")
